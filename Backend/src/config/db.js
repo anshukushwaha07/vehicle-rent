c4fa35638737342c1c4b6d35417a259c738c2973
Anshu Kushwaha <anshu.kush007@gmail.com>
2025-10-20 18:40:33 +0530

    Add correct DB connecting from database

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed", error);
-    process.exit(1);
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1); 
   }
 };
 
